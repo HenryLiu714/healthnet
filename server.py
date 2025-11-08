9107a1119567f0a5cde59a6a17eda5a67ca42b50
ArRstgi <arushrastogi2@outlook.com>
2025-11-08 14:21:48 -0500

    fix(server): added numpy import

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
-from typing import Dict, Optional, Tuple, List
 from collections import OrderedDict
+from typing import Dict, List, Optional, Tuple
 
 import flwr as fl
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -9,7 +10,8 @@ from torch.utils.data import DataLoader
 from torchvision.datasets import CIFAR10
 from torchvision.transforms import Compose, Normalize, ToTensor
 
-DEVICE = torch.device("cpu") # Use CPU for the server
+DEVICE = torch.device("cpu")  # Use CPU for the server
+
 
 # Define the same neural network as the client
 class Net(nn.Module):
@@ -30,13 +32,16 @@ class Net(nn.Module):
         x = F.relu(self.fc2(x))
         return self.fc3(x)
 
+
 def get_parameters(net) -> List[np.ndarray]:
     return [val.cpu().numpy() for _, val in net.state_dict().items()]
+
 
 def set_parameters(net, parameters: List[np.ndarray]):
     params_dict = zip(net.state_dict().keys(), parameters)
     state_dict = OrderedDict({k: torch.tensor(v) for k, v in params_dict})
     net.load_state_dict(state_dict, strict=True)
+
 
 # Define a function for centralized evaluation (optional)
 def get_evaluate_fn(testset):
@@ -46,7 +51,7 @@ def get_evaluate_fn(testset):
         config: Dict[str, fl.common.Scalar],
     ) -> Optional[Tuple[float, Dict[str, fl.common.Scalar]]]:
         net = Net().to(DEVICE)
-        set_parameters(net, parameters) # Update model with the latest parameters
+        set_parameters(net, parameters)  # Update model with the latest parameters
         testloader = DataLoader(testset, batch_size=32)
         criterion = torch.nn.CrossEntropyLoss()
         correct, loss = 0, 0.0
@@ -57,9 +62,11 @@ def get_evaluate_fn(testset):
                 loss += criterion(outputs, labels).item()
                 correct += (torch.max(outputs.data, 1)[1] == labels).sum().item()
         accuracy = correct / len(testloader.dataset)
-        print(f"Server-side evaluation accuracy: {accuracy*100:.2f}%")
+        print(f"Server-side evaluation accuracy: {accuracy * 100:.2f}%")
         return loss, {"accuracy": accuracy}
+
     return evaluate
+
 
 if __name__ == "__main__":
     # 1. Load a small dataset for centralized evaluation
@@ -70,9 +77,9 @@ if __name__ == "__main__":
     # The `evaluate_fn` is optional and used for centralized evaluation
     strategy = fl.server.strategy.FedAvg(
         fraction_fit=1.0,  # Train on 100% of available clients
-        min_fit_clients=2, # Wait for 2 clients to be available for training
-        min_available_clients=2, # Wait for 2 clients to be connected to the server
-        evaluate_fn=get_evaluate_fn(testset), # Pass the evaluation function
+        min_fit_clients=2,  # Wait for 2 clients to be available for training
+        min_available_clients=2,  # Wait for 2 clients to be connected to the server
+        evaluate_fn=get_evaluate_fn(testset),  # Pass the evaluation function
     )
 
     # 3. Start the server
